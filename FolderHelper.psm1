3d78eeef7c29267df641d773e3a50805c1eafe26
Tumanyanchik <tumanyan2000@yandex.ru>
2023-10-21 22:51:38 +0300

    Optimize RemoveEmptyFolders: use recursion instead of loop

--- FolderHelper.psm1
+++ FolderHelper.psm1
@@ -1,34 +1,42 @@
+<#
+    RemoveEmptyFolders - remove empty folders and return List of folders, 
+        which were not deleted from source List of folders.
+    [System.Collections.Generic.List[System.String]] $Folders - folders for cleaning;
+    [System.Collections.Generic.List[System.String]] $Exclude - folders for exclude from cleaning;
+#>
 function RemoveEmptyFolders {
     param(
-        [Parameter (Mandatory=$true)][System.Collections.Generic.List[System.String]] $Folders,
+        [Parameter (Mandatory=$true)] [System.Collections.Generic.List[System.String]] $Folders,
         [System.Collections.Generic.List[System.String]] $Exclude
     )
 
-    [System.Collections.Generic.List[System.String]] $removedFolders = [System.Collections.Generic.List[System.String]]::new()
-    $isFoldersNeedClean = $true
-    while ($Folders -and $isFoldersNeedClean) {
-        foreach ($folder in $Folders){
-            $isFoldersNeedClean = $false
+    [System.Collections.Generic.List[System.String]] $removedFolders = `
+        [System.Collections.Generic.List[System.String]]::new()
 
-            if($folder -in $Exclude){
-                continue
-            }
-
-            if (-not (Get-Item $folder).GetFileSystemInfos()){
-                Remove-Item $folder -Force *> $null
-                $removedFolders.Add($folder)
-                $isFoldersNeedClean = $true
-            }
+    foreach ($folder in $Folders) {
+        if ($folder -in $Exclude) {
+            continue
         }
 
-        foreach ($folder in $removedFolders){
-            if($folder -in $Folders){
-                $Folders.Remove($folder) *> $null
-            }
+        if (-not (Get-Item $folder).GetFileSystemInfos()) {
+            Remove-Item $folder -Force *> $null
+            $removedFolders.Add($folder)
+            $isRemoved = $true
         }
-
-        $removedFolders.Clear()
     }
-    
+
+    foreach ($folder in $removedFolders) {
+        if ($folder -in $Folders){
+            $Folders.Remove($folder) *> $null
+        }
+    }
+
+    $removedFolders.Clear() 
+
+    if ($isRemoved) {
+        $isRemoved = $false
+        $Folders = RemoveEmptyFolders -Folders $Folders -Exclude $Exclude
+    }
+
     return $Folders
 }
